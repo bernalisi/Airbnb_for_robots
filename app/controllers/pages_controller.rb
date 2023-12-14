deb01d0e7a7b240dd2216bdb33ab59d58f267d4a
BERNARDO LISI <bernardo.lisi.work@gmail.com>
2023-12-14 18:18:24 +0100

    Initial commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,12 @@
+class PagesController < ApplicationController
+  skip_before_action :authenticate_user!, only: :home
+
+  def home
+  end
+
+  def dashboard
+    @user_bookings = current_user.bookings
+    @user_robots = current_user.robots
+    @user_venue = @user_b
+  end
+end
